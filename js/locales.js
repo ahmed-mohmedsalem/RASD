e5e363794c7d2ef5688f8a5c4ce959825e03d2fa
ahmed-mohmedsalem <143712468+ahmed-mohmedsalem@users.noreply.github.com>
2025-02-25 23:42:46 +0100

    2 commit

---
+++ js/locales.js
@@ -0,0 +1,10 @@
+export const translations = {
+    en: {
+        welcome: "Welcome to our website!",
+        language: "Language"
+    },
+    ar: {
+        welcome: "مرحبًا بك في موقعنا!",
+        language: "اللغة"
+    }
+};
